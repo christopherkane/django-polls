fdc97dc6b2182cd77b4e3111157824147554a9d2
Christopher Kane <ckane386@gmail.com>
2017-04-06 22:30:40 +0100

    Adding question object retrieval for results and vote views

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/vote.html', {'question': question})
