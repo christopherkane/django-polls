74b2469f2e2eeb6515f698ef6d9123d0f27704c8
Christopher Kane <ckane386@gmail.com>
2017-04-10 21:55:31 +0100

    Adding tests for question detail view, again using Django test client

--- polls/tests.py
+++ polls/tests.py
@@ -53,9 +53,9 @@ class QuestionMethodTests(TestCase):
 
 
 """
-Tests for Question Views, using Djanog's test client
+Tests for Question Index View, using Djanog's test client
 """
-class QuestionViewTests(TestCase):
+class QuestionIndexViewTests(TestCase):
     def test_index_view_with_no_questions(self):
         """
         If no questions exist, an appropriate message should be displayed.
@@ -111,3 +111,27 @@ class QuestionViewTests(TestCase):
             response.context['recent_qs'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+"""
+Tests for Question Detail View, using Djanog's test client
+"""
+class QuestionDetailViewTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
