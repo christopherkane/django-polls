1bb1d0a2bdc160c2df1cb6192e6bfd62386591e0
Christopher Kane <ckane386@gmail.com>
2017-04-12 21:34:01 +0100

    Tweaking comments surrounding static settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,10 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+# Absolute path to the directroy where 'collectstatic' will collect static
+# files for deployment
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+# Extra places for 'collectstatic' to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
